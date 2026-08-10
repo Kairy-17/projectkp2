132cae6222a96cb0c8e40500b44f913d0082c961
adamshafwan-ai <adam.shafwan@mhs.unsoed.ac.id>
2026-08-10 10:27:49 +0700

    fix unencrypted cookie not readable by laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->encryptCookies(except: [
+            'protrack_user_name',
+        ]);
         $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\LanguageMiddleware::class,
